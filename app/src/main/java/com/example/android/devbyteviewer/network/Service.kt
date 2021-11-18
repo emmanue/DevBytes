2474b09df2005fee90476da8b31804d9abaa4dd6
MANU K <brinkcorp@gmail.com>
2021-11-18 17:56:41 +0000

    Increments

--- app/src/main/java/com/example/android/devbyteviewer/network/Service.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/Service.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+ // Separation of concern following best practices
 
 package com.example.android.devbyteviewer.network
 
