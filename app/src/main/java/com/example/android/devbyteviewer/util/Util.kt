5dd90dc9abf911ab11d2d04840c8ebb3364e5dea
MANU K <brinkcorp@gmail.com>
2021-11-20 19:35:55 +0000

    Increment

--- app/src/main/java/com/example/android/devbyteviewer/util/Util.kt
+++ app/src/main/java/com/example/android/devbyteviewer/util/Util.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// Following coding best practices
 
 package com.example.android.devbyteviewer.util
 
